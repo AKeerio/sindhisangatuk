e781a597f345b256d87bf442a9df863caf22e08a
Aijaz1 <aijazkeerio@gmail.com>
2020-04-05 17:01:33 +0100

    Working image gallary

--- src/components/PictureGallary/Pictures.js
+++ src/components/PictureGallary/Pictures.js
@@ -1,39 +1,101 @@
-import React, { useState, useEffect, Fragment } from "react";
-
-import Grid from "@material-ui/core/Grid";
+import React, { useState, useEffect, useCallback } from "react";
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
 import axios from "axios";
-import ThePicture from "./ThePicture";
 
 const API_KEY = `${process.env.REACT_APP_API_KEY_FB}`;
 
 export default function Pictures(props) {
+  var photos = [];
+
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+  
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
 
   const [data, setData] = useState();
-  const album_id=props.albumId;
+  const album_id = props.albumId;
+  
   useEffect(() => {
     axios
       .get(
-        "https://graph.facebook.com/v6.0/"+album_id+"?fields=photos.fields(source)&access_token="+API_KEY
+        "https://graph.facebook.com/v6.0/" +
+          album_id +
+          "?fields=photos.fields(source)&access_token=" +
+          API_KEY
       )
-      .then(({ data }) => {      
+      .then(({ data }) => {
         setData(data);
         console.log(data);
       });
   }, []);
 
   return data ? (
-    <Fragment>
-      <Grid container spacing={8}>
-        {data.photos.data.map((picture, index) => {
-          return (
-            <Grid item xs={12} sm={6} lg={4} key={index}>
-              <ThePicture picture={picture} />
-            </Grid>
-          );
-        })}
-      </Grid>
-    </Fragment>
+    <div>
+      {data.photos.data.map((picture, index) => {
+        photos = [...photos, { src: picture.source, width: 3, height: 2 }];
+      })}
+      {console.log(photos)}
+      <Gallery photos={photos} onClick={openLightbox} />
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map((x) => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title,
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    </div>
   ) : (
-    <div><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>Loading...<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/></div>
+    // <Fragment>
+    //   <Grid container spacing={8}>
+    //     {data.photos.data.map((picture, index) => {
+    //       return (
+    //         <Grid item xs={12} sm={6} lg={4} key={index}>
+    //           <ThePicture picture={picture} />
+    //         </Grid>
+    //       );
+    //     })}
+    //   </Grid>
+    // </Fragment>
+    <div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      Loading...
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
   );
 }
--- src/components/PictureGallary/ThePicture.js
+++ src/components/PictureGallary/ThePicture.js
@@ -1,10 +1,7 @@
 import React from "react";
-import { Link as RouterLink } from "react-router-dom";
-import Link from "@material-ui/core/Link";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
-import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 
 import styles from "assets/jss/material-kit-react/views/profilePage.js";
